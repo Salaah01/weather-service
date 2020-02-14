af9977aaa31c23c26e318094e8be9205efd6ed37
Salaah <salaah_amin@hotmail.co.uk>
2020-02-14 00:12:06 +0000

    migration to handle initial values for Forecast model, unittest to test this works

--- forecast/tests/test_models.py
+++ forecast/tests/test_models.py
@@ -23,38 +23,6 @@ class TestCities(TestCase):
     """
 
 
-def test_cities_test_generator():
-    """Generates tests to be attached to TestCities.
-    The test will check that all cities have a record on the database.
-    The initial input of all cities onto the database is handled by
-    migration 2.
-    """
-
-    def base_test(testPassed, failMessage=None):
-        def test(self):
-            self.assertTrue(testPassed, failMessage)
-        return test
-
-    setattr(TestCities, 'test_1', base_test(1, 1))
-
-    for city in corefunctions.all_cities:
-        # By using .get, either the record or None will be returned.
-        querySet = Cities.objects.get(name=city)
-        testString = f'test_{city}'
-
-        # By using .get when creating querySet, the test in essence has
-        # already been done as it'll either return the record or None.
-        if querySet:
-            setattr(TestCities, testString, base_test(True))
-        else:
-            failMessage = f'{city} not found in the database'
-            setattr(test_cities_test_generator, testString,
-                    base_test(False, failMessage))
-
-
-test_cities_test_generator()
-
-
 class TestForecast(TestCase):
     """Test to ensure that the models.Forecast stores data correctly."""
 
@@ -89,3 +57,61 @@ class TestForecast(TestCase):
             self.assertEquals(testData['temperature'], dbItem.temperature)
         else:
             self.assertTrue(False, 'Test item could not be found.')
+
+
+def test_cities_test_generator():
+    """Generates tests to be attached to TestCities.
+    The test will check that all cities have a record on the database.
+    The initial input of all cities onto the database is handled by
+    migration 2.
+    """
+
+    def base_test(testPassed, failMessage=None):
+        def test(self):
+            self.assertTrue(testPassed, failMessage)
+        return test
+
+    for city in corefunctions.all_cities:
+        # By using .get, either the record or None will be returned.
+        querySet = Cities.objects.get(name=city)
+        testString = f'test_{city}'
+
+        # By using .get when creating querySet, the test in essence has
+        # already been done as it'll either return the record or None.
+        if querySet:
+            setattr(TestCities, testString, base_test(True))
+        else:
+            failMessage = f'{city} not found in the database'
+            setattr(TestCities, testString,
+                    base_test(False, failMessage))
+
+
+def test_forecast_test_generator():
+    """Generates tests to be attached to TestForecast.
+    The test will check that all cities have a record on the Forecast table.
+    The initial input of all cities onto the database is handled by
+    migration 4.
+    """
+
+    def base_test(testPassed, failMessage=None):
+        def test(self):
+            self.assertTrue(testPassed, failMessage)
+        return test
+
+    for city in corefunctions.all_cities:
+        # By using .get, either the record or None will be returned.
+        querySet = Forecast.objects.get(city=city)
+        testString = f'test_{city}'
+
+        # By using .get when creating querySet, the test in essence has
+        # already been done as it'll either return the record or None.
+        if querySet:
+            setattr(TestForecast, testString, base_test(True))
+        else:
+            failMessage = f'{city} not found in the database'
+            setattr(TestForecast, testString,
+                    base_test(False, failMessage))
+
+
+test_cities_test_generator()
+test_forecast_test_generator()
